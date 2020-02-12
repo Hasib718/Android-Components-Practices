2282c60c84c6ea2c10b14afba1c22c0f4851e869
Hasib718 <smalreal14@gmail.com>
2020-02-12 19:27:25 +0600

    Something new

--- First Project/app/src/main/java/com/hasib/firstproject/MainActivity.java
+++ First Project/app/src/main/java/com/hasib/firstproject/MainActivity.java
@@ -2,12 +2,44 @@ package com.hasib.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView background;
+    Button button;
+
+    public void clickFunction(View view) {
+        Log.i("Test", "Button Clicked");
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        Toast.makeText(MainActivity.this, username.getText(), Toast.LENGTH_LONG).show();
+
+        Log.i("Info", username.getText().toString());
+        Log.i("info_pass", password.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Code for changing image on button click
+
+//        background = (ImageView) findViewById(R.id.firstImage);
+//        button = (Button) findViewById(R.id.testButton);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                background.setImageResource(R.drawable.test);
+//            }
+//        });
     }
 }
