03193154c251fc127cee8fdb273cbbb0137d1267
Hasib718 <smalreal14@gmail.com>
2020-02-24 09:46:44 +0600

    GridLayout tracking using getId() method

--- GridLayout/app/src/main/java/com/hasib/gridlayout/MainActivity.java
+++ GridLayout/app/src/main/java/com/hasib/gridlayout/MainActivity.java
@@ -2,9 +2,31 @@ package com.hasib.gridlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer = null;
+
+    public void pressingButton(View view) {
+        int id = view.getId();
+
+        String buttonIdName = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(buttonIdName, "raw", "com.hasib.gridlayout");
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+
+        mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+
+        Log.i("Button Tapped", Integer.toString(resourceId));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
