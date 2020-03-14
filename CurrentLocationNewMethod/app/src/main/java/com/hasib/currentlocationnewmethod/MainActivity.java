e9b51986b255ba2326885d465769e486874949bd
Hasib718 <smalreal14@gmail.com>
2020-03-14 13:45:56 +0600

    Updating Location Frequently

--- CurrentLocationNewMethod/app/src/main/java/com/hasib/currentlocationnewmethod/MainActivity.java
+++ CurrentLocationNewMethod/app/src/main/java/com/hasib/currentlocationnewmethod/MainActivity.java
@@ -3,12 +3,21 @@ package com.hasib.currentlocationnewmethod;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -24,12 +33,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 101;
     private ImageButton gettingLocationButoon;
-    View nview;
+    private GoogleMap googleMap;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private Geocoder geocoder;
+    String addressLineOuterScope;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -37,6 +54,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             case REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     configureButton();
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, listener);
+                    }
                 }
                 break;
         }
@@ -49,6 +69,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         gettingLocationButoon = (ImageButton) findViewById(R.id.gettingLocationButton);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
         configureButton();
     }
 
@@ -68,6 +90,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                         {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
                 return;
             }
+
             Task<Location> task = fusedLocationProviderClient.getLastLocation();
             task.addOnSuccessListener(new OnSuccessListener<Location>() {
                 @Override
@@ -89,11 +112,64 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            List<Address> addressListOuterScope = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
+
+            if (addressListOuterScope != null && addressListOuterScope.size() > 0) {
+                Log.i("PlaceInfo", addressListOuterScope.get(0).toString());
+                addressLineOuterScope = addressListOuterScope.get(0).getAddressLine(0);
+                Toast.makeText(MainActivity.this, addressLineOuterScope, Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am Here");
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(addressLineOuterScope);
 
 //        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
         googleMap.addMarker(markerOptions);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(MainActivity.this, location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_LONG).show();
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (addressList != null && addressList.size() > 0) {
+                        Log.i("PlaceInfo", addressList.get(0).toString());
+                        String addressLine = addressList.get(0).getAddressLine(0);
+                        Toast.makeText(MainActivity.this, addressLine, Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, listener);
+        }
     }
 }
