65c7ab6c6f7659710a921c9e7b1170afc9d0e33c
Hasib718 <smalreal14@gmail.com>
2020-03-17 05:16:11 +0600

    Alert Message

---
+++ AlertMessageDemo/app/src/main/java/com/hasib/alertmessagedemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.hasib.alertmessagedemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.englishItem:
+                sharedPreferences.edit().putString("language", "English").apply();
+            case R.id.spanishItem:
+                sharedPreferences.edit().putString("language", "Spanish").apply();
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        sharedPreferences = this.getSharedPreferences("com.hasib.alertmessagedemo", Context.MODE_PRIVATE);
+        String language = sharedPreferences.getString("language", "");
+
+        if (language == "") {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose a language")
+                    .setMessage("Which language would you like?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            sharedPreferences.edit().putString("language", "English").apply();
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            sharedPreferences.edit().putString("language", "Spanish").apply();
+                        }
+                    })
+                    .show();
+        } else {
+            textView.setText(sharedPreferences.getString("language", ""));
+        }
+    }
+}
