c65979b0f9c5784c7df55f363050fda48e18d52b
Hasib718 <smalreal14@gmail.com>
2020-03-19 02:29:09 +0600

    SQLite Database

---
+++ DatabaseDemo/app/src/main/java/com/hasib/databasedemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hasib.databasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Hasib', 22)");
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Toufiq', 20)");
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Wadud', 21)");
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Antor', 19)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+            while(c != null) {
+                Log.d(TAG, "onCreate: name "+c.getString(nameIndex));
+                Log.d(TAG, "onCreate: age "+c.getInt(ageIndex));
+
+                c.moveToNext();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
