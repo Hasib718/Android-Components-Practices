8ac7163a1a7ebbd1bd6cbe7a3a7717906b4e6a61
Hasib718 <smalreal14@gmail.com>
2020-04-08 20:38:06 +0600

    Place API doesn't work out
    Place api doesn't work out because of not having a billing account on google cloud platform

--- PlacesApi/app/src/main/java/com/hasib/placesapi/MainActivity.java
+++ PlacesApi/app/src/main/java/com/hasib/placesapi/MainActivity.java
@@ -3,10 +3,13 @@ package com.hasib.placesapi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ScrollView;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -16,6 +19,7 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private PlacesClient placesClient;
 
@@ -35,10 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
         autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.LAT_LNG, Place.Field.ADDRESS, Place.Field.NAME));
 
+        autocompleteSupportFragment.setCountries("BD");
+
         autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(@NonNull Place place) {
+                final LatLng latLng = place.getLatLng();
+                final String address = place.getAddress();
 
+                Log.d(TAG, "onPlaceSelected: latlng"+latLng.latitude + " " + latLng.longitude);
+                Log.d(TAG, "onPlaceSelected: address "+address);
             }
 
             @Override
@@ -46,14 +56,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        /**
-         *
-         *
-         * testing branch ..
-         * now commit & push this branch to git
-         *
-         *
-         */
     }
 }
