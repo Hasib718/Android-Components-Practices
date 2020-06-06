579e5c2e823686a62b6d9a9585d78d4444d003b5
S M Al Hasib <47137092+hasib718@users.noreply.github.com>
2020-06-07 02:09:37 +0600

    notification receiver & action button

--- NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.java
+++ NotificationDemo/app/src/main/java/com/example/notificationdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.notificationdemo;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -8,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
 import static com.example.notificationdemo.NotificationDemo.CHANNEL_1_ID;
 import static com.example.notificationdemo.NotificationDemo.CHANNEL_2_ID;
@@ -32,12 +36,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendOnChannel1(View v) {
+        Intent activityIntent = new Intent(this, TestingActivity2.class);
+
+        /**
+         * this three lines will not only start activity but also the parent activities on back press, but the applications activity hierarchy must be well defined.
+         * Like this ---
+         *         <activity android:name=".TestingActivity2"
+         *             android:parentActivityName=".TestingActivity"></activity>
+         */
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(activityIntent);
+        PendingIntent contentIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        /**
+         * If this particular line is added then, tapping on the notification will start the only activity which is called. back button will take to the Main Activity
+         */
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", editTextMessage.getText().toString());
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(editTextTitle.getText().toString())
                 .setContentText(editTextMessage.getText().toString())
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
 
         notificationManagerCompat.notify(1, notification);
