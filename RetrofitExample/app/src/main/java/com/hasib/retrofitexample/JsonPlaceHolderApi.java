816da3710d9da915df562fbbaa8ee19706b7f458
S M Al Hasib <smalreal14@gmail.com>
2021-01-13 19:17:39 +0600

    Retrofit GET example

---
+++ RetrofitExample/app/src/main/java/com/hasib/retrofitexample/JsonPlaceHolderApi.java
@@ -0,0 +1,10 @@
+package com.hasib.retrofitexample;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
