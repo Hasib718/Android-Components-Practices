4184abf2c8cf2570cd5fcc2c267955c67ef5bee0
S M Al Hasib <smalreal14@gmail.com>
2021-07-23 21:39:49 +0600

    App repository and Offline caching

---
+++ _Kotlin-Camouflage/DevBytes/app/src/main/java/com/hasib/devbytes/database/DatabaseEntities.kt
@@ -0,0 +1,34 @@
+package com.hasib.devbytes.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hasib.devbytes.domain.DevByteVideo
+
+/**
+ * DatabaseVideo represents a video entity in the database.
+ */
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+
+/**
+ * Map DatabaseVideos to domain entities
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<DevByteVideo> {
+    return map {
+        DevByteVideo(
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
+    }
+}
