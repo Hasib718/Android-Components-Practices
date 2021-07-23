4442619d47ab5c21020f2bfc086b21d704f83a21
S M Al Hasib <smalreal14@gmail.com>
2021-07-24 00:19:15 +0600

    PeriodicTask with WorkManager

--- _Kotlin-Camouflage/DevBytes/app/src/main/java/com/hasib/devbytes/DevByteApplication.kt
+++ _Kotlin-Camouflage/DevBytes/app/src/main/java/com/hasib/devbytes/DevByteApplication.kt
@@ -1,12 +1,20 @@
 package com.hasib.devbytes
 
 import android.app.Application
+import androidx.work.*
+import com.hasib.devbytes.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -16,6 +24,36 @@ class DevByteApplication : Application() {
      */
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    /**
+     * Setup WorkManager background job to 'fetch' new network data daily.
+     */
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .setRequiresCharging(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
