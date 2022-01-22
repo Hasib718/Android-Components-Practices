50da4e0ba7f5c7dbf69d702150ed365b6f79a34d
S M Al Hasib <smalreal14@gmail.com>
2022-01-22 16:57:28 +0600

    Simple UI using compose basics

--- _Kotlin-Camouflage/First-Compose/app/src/main/java/com/hasib/firstcompose/MainActivity.kt
+++ _Kotlin-Camouflage/First-Compose/app/src/main/java/com/hasib/firstcompose/MainActivity.kt
@@ -4,19 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.hasib.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
